8c3febe2c9e67d48c18c93f3ed3b80bf63f1dd38
Gledson Lucas <gledsonlucas111@gmail.com>
2022-03-09 10:40:19 -0300

    estado global

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import GlobalState from './Global/GlobalState';
 import Router from './Routes/Router';
+
 
 function App() {
 
   return (
-    <div >
-
+    <GlobalState>
       <Router />
-
-    </div>
+    </GlobalState>
   )
 
 }

635918e87f2609af10972c9e44d0912a9dd80143
Gledson Lucas <gledsonlucas111@gmail.com>
2022-03-09 11:02:44 -0300

    passando por props da home pro componente cardPokemon

--- pokedex/src/Pages/Home/Home.js
+++ pokedex/src/Pages/Home/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import CardsPokemons from "../../Components/CardsPokemons/CardsPokemons";
+import { BASE_URLING } from "../../Constants/Urls";
+import GlobalStateContext from "../../Global/GlobalStateContext";
+
 
 function Home(){
+    const {listaPoke} = useContext(GlobalStateContext)
+
+    const listaMapeada = listaPoke.results && listaPoke.results.map((pokes, i) => {
+        return (
+            <CardsPokemons
+                key={pokes.name}
+                imagem={`${BASE_URLING}/${i + 1}.gif`}
+                nome={pokes.name}
+            />
+            )
+        })    
     return(
         <div>
-            Home
-            <CardsPokemons/>
+           {listaMapeada}
         </div>
     )
 }

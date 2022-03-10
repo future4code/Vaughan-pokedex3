4a9cd727f34a8398e55a14a432cf087db45b4348
GeysonMes <geysonsousa17@gmail.com>
2022-03-10 19:04:25 -0300

    salvando alteracoes

--- pokedex/src/Pages/PaginaPokedex/PaginaPokedex.js
+++ pokedex/src/Pages/PaginaPokedex/PaginaPokedex.js
@@ -1,26 +1,30 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import CardsPokemons from "../../Components/CardsPokemons/CardsPokemons";
 import { BASE_URLING } from "../../Constants/Urls";
 import GlobalStateContext from "../../Global/GlobalStateContext";
+import { irParaHome } from "../../Routes/Coordenadas";
 
 
 function PaginaPokedex() {
   const { pokemons, setPokemons } = useContext(GlobalStateContext)
-  const listPoke = pokemons.map((pikachu,i) => {
+  const listPoke = pokemons.map((pikachu, i) => {
+  const navigate = useNavigate()
 
     return (
       <div>
         <CardsPokemons
-           nome={pikachu.name}
-          imagem={`${BASE_URLING}/${i+1}.gif`}
-          />
+          nome={pikachu.name}
+          imagem={`${BASE_URLING}/${i + 1}.gif`}
+        />
       </div>
-)
+    )
   })
   console.log(pokemons)
   return (
     <div>
       {listPoke}
+      <button onClick={() => irParaHome(navigate)}>ir para pokedex</button>
     </div>
 
   );

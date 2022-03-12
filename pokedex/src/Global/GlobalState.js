7b18fd42d11e9753906bb6ebb3f9382d61194ab9
GeysonMes <geysonsousa17@gmail.com>
2022-03-12 20:46:45 -0300

    remmover funcao

--- pokedex/src/Global/GlobalState.js
+++ pokedex/src/Global/GlobalState.js
@@ -1,35 +1,52 @@
+import { Alert } from "@chakra-ui/react";
 import React, {useState} from "react"
 import { BASE_URL } from "../Constants/Urls";
 import useRequestData from "../Hooks/UseRequestData";
 import GlobalStateContext from "./GlobalStateContext"
 
 function GlobalState(props) {
-    const [listaPoke] = useRequestData([], `${BASE_URL}/pokemon`)
+    const [listaPoke,setListaPoke] = useRequestData([], `${BASE_URL}/pokemon`)
     const [pokemons, setPokemons] = useState([])
-    
-    const adicionarPokemon = (pokes) => {
+     
+    // const adicionarPokemon = (pokes) => {
 
-        const pokeAdd= [...pokemons]
-        if (pokes ) {
-            pokeAdd.push({...pokes});
-          alert("deu certo")
+    //     const pokeAdd= [...pokemons]
+    //     if (pokes ) {
+    //         pokeAdd.push({...pokes});
+    //       alert("deu certo")
           
-        }
-        setPokemons(pokeAdd)
-        console.log(pokeAdd)
-      };
+    //     }
+    //     setPokemons(pokeAdd)
+    //     console.log(pokeAdd)
+    //   };
 
-    const removerPokemon =(index)=>{
-      const novo = pokemons.slice()
-      novo.splice(index, 1)
-      setPokemons({pokemons})
-      console.log(pokemons)
+    // const removerPokemon =(index)=>{
+    //   const novo = pokemons.slice()
+    //   novo.splice(index, 1)
+    //   setPokemons({pokemons})
+    //   console.log(pokemons)
       
       
-    }
+    // }
+    const adicionarPokemon = (pokemon) => {
+      setPokemons([...pokemons, pokemon]);
+      const newPokemonList = listaPoke.filter((p) => p.name !== pokemon.name);
+      setListaPoke(newPokemonList);
+        alert('As senhas não conferem!' );
+      
+      
+    };
+  
+    const removerPokemon= (pokemon) => {
+      const newPokedex = pokemons.filter((p) => p.name !== pokemon.name);
+      setPokemons(newPokedex);
+      const newPokemonList = [...listaPoke, pokemon];
+      setListaPoke(newPokemonList);
+    };
+    
 
     return (
-        <GlobalStateContext.Provider value={{ listaPoke, setListaPoke,pokemons, setPokemons,adicionarPokemon, removerPokemon}}>
+        <GlobalStateContext.Provider value={{ listaPoke,setListaPoke,pokemons, setPokemons,adicionarPokemon, removerPokemon}}>
             {props.children}
         </GlobalStateContext.Provider>
     )

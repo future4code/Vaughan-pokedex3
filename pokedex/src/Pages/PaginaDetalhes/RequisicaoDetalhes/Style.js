90ad37188681463a89654a1e108f18b9f355ad7b
AnaKarine27 <karinesantos364@gmail.com>
2022-03-13 13:27:25 -0300

    pagina de erro

--- pokedex/src/Pages/PaginaDetalhes/RequisicaoDetalhes/Style.js
+++ pokedex/src/Pages/PaginaDetalhes/RequisicaoDetalhes/Style.js
@@ -215,5 +215,3 @@ export const MovesContainer = styled.div`
     padding-bottom: 20px;
   }
 `
-
-
--- pokedex/src/Pages/PaginaErro/PaginaErro.js
+++ pokedex/src/Pages/PaginaErro/PaginaErro.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import {ErroImg, Botao, ContainerErro} from './Styled'
+import Erro from '../../Assets/2.png'
+import { irParaHome } from '../../Routes/Coordenadas'
+import { useNavigate } from 'react-router-dom'
 
 function PaginaDetalhes() {
+    const navigate = useNavigate()
 
     return (
-        <div>
-            <h1>EROOOOOOOOOOOOOOOOOOO</h1>
-        </div>
+        <ContainerErro>
+            <ErroImg src={Erro} alt="Imagem página de erro!"/>
+            <Botao onClick={() => irParaHome(navigate)}>IR PARA HOME 🏠</Botao>
+        </ContainerErro>
     )
 }
 
